ce6f6192d61fcff3cec114a8b6fe4a510fd246ca
ilya <i.novikov@restream.rt.ru>
2019-10-06 12:10:23 +0300

    fix

--- models/models_test.go
+++ models/models_test.go
@@ -1,13 +1,1 @@
 package models
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestGetGitlabID(t *testing.T) {
-	mr := "https://git.itv.restr.im/itv-backend/itv-api-ng/merge_requests/2392/pipelines#db7a71f25fb8f66d309e5c4726d920ecdd492523"
-	_, err := GetGitlabID(mr)
-	assert.Error(t, err)
-}
